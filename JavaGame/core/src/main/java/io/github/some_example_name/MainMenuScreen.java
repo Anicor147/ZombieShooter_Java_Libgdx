fb30e59409996ec2d1e89082ab6ed9132788c23f
Anthony <antho.nicu.cor147@gmail.com>
2024-11-21 21:34:11 -0500

    textfield + boutton fonctionne

--- JavaGame/core/src/main/java/io/github/some_example_name/MainMenuScreen.java
+++ JavaGame/core/src/main/java/io/github/some_example_name/MainMenuScreen.java
@@ -3,7 +3,15 @@ package io.github.some_example_name;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.FocusListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.awt.*;
@@ -11,13 +19,56 @@ import java.awt.*;
 public class MainMenuScreen implements Screen {
 
     final TheGame game;
+    private Stage stage;
+    private Skin skin;
 
+    TextField usernameTF;
+    TextField passwordTF;
 
-    TextField username;
     public MainMenuScreen(final TheGame game) {
         this.game = game;
-        username = new TextField();
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        stage = new Stage();
+        usernameTF = new TextField("Enter a Username", skin);
+        passwordTF = new TextField("Enter a Password", skin);
+        usernameTF.setPosition(50, 100);
+        usernameTF.setSize(200, 40);
+        passwordTF.setPosition(50,50 );
+        passwordTF.setSize(200,40);
+        TextButton button = new TextButton("Login", skin);
+        button.setPosition(400, 75);
+        button.setSize(150, 50);
 
+        usernameTF.addListener(new FocusListener() {
+            @Override
+            public void keyboardFocusChanged(FocusEvent event, Actor actor, boolean focused) {
+                if (focused) {
+                    usernameTF.setText("");
+                }
+            }
+        });
+
+        passwordTF.addListener(new FocusListener() {
+            @Override
+            public void keyboardFocusChanged(FocusEvent event, Actor actor, boolean focused) {
+                if (focused) {
+                    passwordTF.setText("");
+                }
+            }
+        });
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //manque juste la logic avec base de donnée
+                game.setScreen(new Main(game));
+            }
+        });
+
+        stage.addActor(passwordTF);
+        stage.addActor(usernameTF);
+        stage.addActor(button);
+
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -35,16 +86,12 @@ public class MainMenuScreen implements Screen {
         game.batch.begin();
         game.font.getData().setScale(1f);
         game.font.setColor(Color.BLUE);
-        game.font.draw(game.batch, "Welcome to zombieShooter!!!", 220, 400); // Adjust coordinates as needed
-        game.font.draw(game.batch, "Tap anywhere to begin!", 250, 100);
-        username.setText("Enter Username here");
-        username.setLocation(100, 200);
+        game.font.draw(game.batch, "Welcome to zombieShooter!!!", 220, 400);
         game.batch.end();
+        stage.act(delta);
+        stage.draw();
 
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new Main(game));
-            dispose();
-        }
+
     }
 
     @Override
@@ -71,4 +118,5 @@ public class MainMenuScreen implements Screen {
     public void dispose() {
 
     }
+
 }
